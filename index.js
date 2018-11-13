d980753a16fefe76195a49bdb916dff01b3e65dd
Sissel Fladby <sissel.fladby@nav.no>
2018-11-13 15:53:34 +0100

    Tar i bruk vault for secrets

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 const express = require('express');
+const sanityClient = require('@sanity/client');
+const fs = require('file-system');
 
 const app = express();
-
-app.get('/soknadsveiviserproxy/isAlive', (req, res) =>
+app.get('/isAlive', (req, res) =>
     res.sendStatus(200));
 
-app.get('/soknadsveiviserproxy/isReady', (req, res) =>
+app.get('/isReady', (req, res) =>
     res.sendStatus(200));
 
-app.get('/soknadsveiviserproxy', (req, res) =>
-    res.sendStatus(200));
+app.get('/', (req, res) => {
+    const secretsFilePath = '/var/run/secrets/nais.io/vault';
+    const projectIDPath = secretsFilePath + '/sanity.projectID';
+    const tokenPath = secretsFilePath + '/sanity.token';
+
+    const projectID = fs.readFileSync(projectIDPath, 'utf8');
+    const token = fs.readFileSync(tokenPath, 'utf8');
+
+    const client = sanityClient({
+        projectId: projectID,
+        dataset: 'skjemaveileder',
+        token: token,
+        useCdn: false,
+    });
+
+    const skjemaQuery = `*[_type == "skjema" && ` +
+        `!(_id in path("drafts.**"))]{emneord, malgruppe, skjemanummer,` +
+        `tema, "navn": navn.nb, "pdf": pdf.nb}`;
+
+    client.fetch(skjemaQuery).then((docs) => {
+        console.log(docs[0]);
+        res.sendStatus(200);
+    }).catch((error) => console.log(error));
+});
 
 app.listen(8080, () => {
     console.log(`App listening on port: 8080`);
