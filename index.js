ee4ad28f66218d24bcf7e27a185b3b4897876e6b
Sissel Fladby <sisfla@gmail.com>
2019-02-12 09:45:51 +0100

    Legg til spørringer mot sanity

--- index.js
+++ index.js
@@ -1,34 +1,76 @@
+const sporringer = require('./sporringer');
+
 const express = require('express');
 const sanityClient = require('@sanity/client');
 const fs = require('file-system');
 
 const app = express();
-app.get('/soknadsveiviserproxy/isAlive', (req, res) =>
-    res.sendStatus(200));
 
-app.get('/soknadsveiviserproxy/isReady', (req, res) =>
-    res.sendStatus(200));
-
-app.get('/soknadsveiviserproxy', (req, res) => {
+if (process.env.NODE_ENV === 'production') {
     const secretsFilePath = '/var/run/secrets/nais.io/vault';
     const projectIDPath = secretsFilePath + '/sanity.projectID';
     const tokenPath = secretsFilePath + '/sanity.token';
 
     const projectID = fs.readFileSync(projectIDPath, 'utf8');
     const token = fs.readFileSync(tokenPath, 'utf8');
-    const client = sanityClient({
+    const client = sanityClient({ // eslint-disable-line no-unused-vars
         projectId: projectID,
         dataset: 'skjemaveileder',
         token: token,
         useCdn: false,
     });
+}
 
-    const skjemaQuery = `*[_type == "skjema"]`;
+app.get('/soknadsveiviserproxy/isAlive', (req, res) =>
+    res.sendStatus(200));
 
-    client.fetch(skjemaQuery).then((docs) => {
-        console.log(docs);
+app.get('/soknadsveiviserproxy/isReady', (req, res) =>
+    res.sendStatus(200));
+
+app.get('/soknadsveiviserproxy/allekategorier', (req, res) => {
+    client.fetch(sporringer.underkategori(req.query.locale)).then((docs) => {
+        res.send(docs);
     }).catch((error) => console.log(error));
 });
+
+app.get('/soknadsveiviserproxy/underkategori', (req, res) => {
+    client.fetch(
+        sporringer.underkategori(
+            req.query.kategori, req.query.underkategori, req.query.locale
+        )
+    ).then((docs) => {
+        res.send(docs);
+    }).catch((error) => console.log(error));
+});
+
+app.get('/soknadsveiviserproxy/soknadsobjekt', (req, res) => {
+    client.fetch(
+        sporringer.soknadsobjektsQuery(
+            req.query.kategori, req.query.underkategori
+        )
+    ).then((docs) => {
+        res.send(docs);
+    }).catch((error) => console.log(error));
+});
+
+app.get('/soknadsveiviserproxy/alleskjemaer', (req, res) => {
+    client.fetch(sporringer.alleskjemaerQuery)
+        .then((docs) => {
+            res.send(docs);
+        })
+        .catch((error) => console.log(error));
+});
+
+app.get('/soknadsveiviserproxy/samlet', (req, res) => {
+    client.fetch(sporringer.samleQuery)
+        .then((docs) => {
+            res.send(docs);
+        })
+        .catch((error) => console.log(error));
+});
+
+app.get('/soknadsveiviserproxy', (req, res) =>
+    res.sendStatus(200));
 
 app.listen(8080, () => {
     console.log(`App listening on port: 8080`);
