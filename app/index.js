66810255ff49dcf2d103eb281c269defbceeca06
Morten Jansrud <morten@founder.no>
2019-02-13 15:03:47 +0100

    CORS tilgang i dev. Minimale kode-forenklinger

--- app/index.js
+++ app/index.js
@@ -5,55 +5,62 @@ const createSanityClient = require('./createSanityClient');
 const app = express();
 const sanityClient = createSanityClient();
 
-app.get('/soknadsveiviserproxy/isAlive', (req, res) =>
-    res.sendStatus(200));
+// Allow access from localhost in development
+if (process.env.NODE_ENV !== 'production') {
+  app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
+  });
+}
 
-app.get('/soknadsveiviserproxy/isReady', (req, res) =>
-    res.sendStatus(200));
+app.get('/soknadsveiviserproxy/isAlive', (req, res) => res.sendStatus(200));
+app.get('/soknadsveiviserproxy/isReady', (req, res) => res.sendStatus(200));
 
-app.get('/soknadsveiviserproxy/allekategorier', (req, res) => {
-    sanityClient.fetch(sporringer.alleKategorier()).then((docs) => {
-        res.send(docs);
-    }).catch((error) => console.log(error));
-});
+app.get('/soknadsveiviserproxy/allekategorier', (req, res) =>
+  sanityClient
+    .fetch(sporringer.alleKategorier())
+    .then(res.send)
+    .catch(console.log)
+);
 
-app.get('/soknadsveiviserproxy/underkategori', (req, res) => {
-    sanityClient.fetch(
-        sporringer.underkategori(req.query.kategori, req.query.underkategori)
-    ).then((docs) => {
-        res.send(docs);
-    }).catch((error) => console.log(error));
-});
+app.get('/soknadsveiviserproxy/underkategori', (req, res) =>
+  sanityClient
+    .fetch(
+      sporringer.underkategori(req.query.kategori, req.query.underkategori)
+    )
+    .then(res.send)
+    .catch(console.log)
+);
 
-app.get('/soknadsveiviserproxy/soknadsobjekt', (req, res) => {
-    sanityClient.fetch(
-        sporringer.soknadsobjektsQuery(
-            req.query.kategori, req.query.underkategori
-        )
-    ).then((docs) => {
-        res.send(docs);
-    }).catch((error) => console.log(error));
-});
+app.get('/soknadsveiviserproxy/soknadsobjekt', (req, res) =>
+  sanityClient
+    .fetch(
+      sporringer.soknadsobjektsQuery(
+        req.query.kategori,
+        req.query.underkategori
+      )
+    )
+    .then(res.send)
+    .catch(console.log)
+);
 
 app.get('/soknadsveiviserproxy/alleskjemaer', (req, res) => {
-    sanityClient.fetch(sporringer.alleskjemaerQuery())
-        .then((docs) => {
-            res.send(docs);
-        })
-        .catch((error) => console.log(error));
+  sanityClient
+    .fetch(sporringer.alleskjemaerQuery())
+    .then(res.send)
+    .catch(console.log);
 });
 
 app.get('/soknadsveiviserproxy/samlet', (req, res) => {
-    sanityClient.fetch(sporringer.samleQuery())
-        .then((docs) => {
-            res.send(docs);
-        })
-        .catch((error) => console.log(error));
+  sanityClient
+    .fetch(sporringer.samleQuery())
+    .then(res.send)
+    .catch(console.log);
 });
 
-app.get('/soknadsveiviserproxy', (req, res) =>
-    res.sendStatus(200));
-
-app.listen(8080, () => {
-    console.log(`App listening on port: 8080`);
-});
+app.get('/soknadsveiviserproxy', (req, res) => res.sendStatus(200));
+app.listen(8080, () => console.log(`App listening on port: 8080`));
