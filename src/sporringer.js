79c5d05be0cf3ae102fb0515fe1119ed2378a5b8
Sissel Fladby <sisfla@gmail.com>
2019-08-09 13:00:40 +0200

    Skulle være med i forrige commit

--- src/sporringer.js
+++ src/sporringer.js
@@ -109,7 +109,7 @@ const alleVedlegg = () =>
 const samlet = () =>
   `*[_type == "kategori" && !(_id in path("drafts.**"))]
         {"tittel": tittel, urlparam, _id, domene, underkategorier[]
-            {_id, "navn": navn, inngangtilsoknadsdialog, urlparam, soknadslenker[]->{lenke, navn, _id},
+            {_id, "navn": navn, inngangtilsoknadsdialog, urlparam, soknadslenker[]->{lenke, navn, _id, beskrivelse},
                 soknadsobjekter[]->
                 {_id, digitalinnsending, "navn": navn, tema, urlparam, beskrivelse,
                     innsendingmate{spesifisertadresse->, skanning, visenheter}, hovedskjema->, "vedleggtilsoknad":
